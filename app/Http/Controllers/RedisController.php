39307ab1bd0884f604856cf7a32e433eec69075f
v138468 <v138468@gmail.com>
2022-03-30 15:45:21 +0800

    test redis

--- app/Http/Controllers/RedisController.php
+++ app/Http/Controllers/RedisController.php
@@ -14,6 +14,7 @@ class RedisController extends Controller
     }
     public function get(Request $request)
     {
+        return gettype(Redis::get("abc"));
         dd(Redis::get("abc"));
     }
 }
