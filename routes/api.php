97e53b0b7cea5213fa9f226b681f1da0f5c98038
v138468 <v138468@gmail.com>
2022-04-01 13:36:40 +0800

    i

--- routes/api.php
+++ routes/api.php
@@ -20,7 +20,7 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
 
-Route::post("test", function (Request $request) {
+Route::any("test", function (Request $request) {
     if ($user =  User::find(1)) {
         return $user;
     } else {
